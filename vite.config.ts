fc3f3c76f2b6ea3ec675d06e16558306a2e6999e
Sachin Sankar <mail.sachinsankar@gmail.com>
2026-04-23 21:46:23 +0530

    chore: Init Project

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite-plus";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+});
